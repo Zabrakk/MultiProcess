5b0f72c96ffbfb2fde3a1d18cc191f7da0489a91
Zabrakk <ak4.jarvenpaa@gmail.com>
2021-03-07 19:01:00 +0200

    CalcZNCC working

--- As5_OpenCL/kernel.cl
+++ As5_OpenCL/kernel.cl
@@ -5,39 +5,91 @@ const sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE |
 
 __kernel void resize_and_grayscale(__read_only image2d_t src, __write_only image2d_t dst) {
 	// Reduce given images size by a factor of four
+	// Then grayscale it
 	
-	// The old width and height have been provided with the source image
-	//int w = get_image_width(src); // 735
-	
-	// Get current coordinates
+	// Get current x and y
 	int x = get_global_id(0);
 	int y = get_global_id(1);
-	// Coordinates for every fourth pixel in the source image
+	// Calculate coordinate for the source image
 	int2 coords_src = {x*4, y*4};
 	// Obtain R,G,B,A value from the coordinate
 	uint4 values = read_imageui(src, sampler, coords_src);
 	
-	// Grayscale
+	// Grayscale the pixel values
 	uint gray_val = (uint) (values.s0*0.299 + values.s1*0.587 + values.s2*0.114);
-
-	printf("%d %d\n", x, y);
 	
+	// Write grayscaled value to destination image
 	int2 coord_dst = {x, y};
-	write_imageui(dst, coord_dst, gray_val);
-	
-	// Write new value
-	//dst[y*735+x] = convert_uchar(gray_val);
-	
+	write_imageui(dst, coord_dst, gray_val);	
 }
 
-
-						  
-						  
-__kernel void to_grayscale(__global const unsigned char* img, __global unsigned char* result) {
-	// Source: "'Standard' RGB to Grayscale Conversion" - https://stackoverflow.com/questions/17615963/standard-rgb-to-grayscale-conversion
-	// Turning the image into grayscale by summing red, green, and blue while applying different weights to each one
+__kernel void calc_zncc(__global const unsigned char* img_left, __global const unsigned char* img_right, __global unsigned char* dst, unsigned int w, unsigned int h, int window_y, int window_x, int min_disparity, int max_disparity) {
+	// Calculates ZNCC between two given images
 	
-	int id = get_global_id(0);
-	//           Red               Green               Blue
-	result[id] = img[id*3]*0.299 + img[id*3+1]*0.587 + img[id*3+2]*0.114;
+	int x = get_global_id(0);
+	int y = get_global_id(1);
+	
+	int window_size = window_y * window_x;
+	int d, win_y, win_x;
+	
+	float lw_mean, rw_mean; // Left and right image mean
+	float lw_mean_diff, rw_mean_diff; // Pixel difference from the mean
+	float lower_sum_0, lower_sum_1, upper_sum;
+	float zncc_val, max_sum, best_disparity;
+	
+	max_sum = -1; // Start with a small number, so values can update
+	best_disparity = max_disparity;
+	
+	for (d = min_disparity; d < max_disparity; d++) { // Loop to maximum disparity value
+		// Reset
+		lw_mean = 0, rw_mean = 0;
+		// Mean for each window. Based on the equation, window_x & window_y should be divided by 2
+		for (win_y = -window_y / 2; win_y < window_y / 2; win_y++) {
+			for (win_x = -window_x / 2; win_x < window_x / 2; win_x++) {
+				// Make sure we are inside the image boundries
+				if (win_y + y < 0 || win_y + y >= h || win_x + x < 0 || win_x + x - d < 0 || win_x + x >= w || win_x + x - d >= w) {
+					// Outside of image, go to next iteration
+					continue;
+				}
+				// Add current pixel value
+				lw_mean += img_left[(win_y + y) * w + (win_x + x)];
+				rw_mean += img_right[(win_y + y) * w + (win_x + x - d)];
+			}
+		}
+		// Calculate the window means by dividing summed values with the window's size
+		lw_mean = lw_mean / window_size;
+		rw_mean = rw_mean / window_size;
+
+		//Reset
+		upper_sum = 0, lower_sum_0 = 0, lower_sum_1 = 0, zncc_val = 0;
+
+		// Calculate ZNCC using the same window loops
+		for (win_y = -window_y / 2; win_y < window_y / 2; win_y++) {
+			for (win_x = -window_x / 2; win_x < window_x / 2; win_x++) {
+				// Make sure we are inside the image boundries
+				if (win_y + y < 0 || win_y + y >= h || win_x + x < 0 || win_x + x - d < 0 || win_x + x >= w || win_x + x - d >= w) {
+					// Outside of image, go to next iteration
+					continue;
+				}
+				// Get pixel mean differences for both images
+				lw_mean_diff = img_left[(win_y + y) * w + (win_x + x)] - lw_mean;
+				rw_mean_diff = img_right[(win_y + y) * w + (win_x + x - d)] - rw_mean;
+				// Lower Sum calculation
+				lower_sum_0 += lw_mean_diff * lw_mean_diff;
+				lower_sum_1 += rw_mean_diff * rw_mean_diff;
+				// Upper Sum calculation
+				upper_sum += lw_mean_diff * rw_mean_diff;
+			}
+		}
+		// Calculating the ZNCC value with upper and lower sum
+		zncc_val = upper_sum / (sqrt(lower_sum_0) * sqrt(lower_sum_1));
+		// Check if maximum sum and best disparity should be updated based on current zncc value
+		if (zncc_val > max_sum) {
+			best_disparity = d;
+			max_sum = zncc_val;
+		}
+	}
+	// Add resulting best disparity value to the disparity map
+	dst[y * w + x] = convert_uchar(abs((int)best_disparity)); // Use absolute value of the disparity
+	
 }
--- As5_OpenCL/main.cpp
+++ As5_OpenCL/main.cpp
@@ -14,19 +14,29 @@
 #include "ImageFunctions.h"
 #include "OpenCLFunctions.h"
 
-#define WINDOW_Y 13 
-#define WINDOW_X 11 
-#define MIN_DISPARITY 0
-#define MAX_DISPARITY 65 // Scaled down. 260/4 as stated in the Assignment
-#define THRESHOLD 3
 
 #define KERNEL_FILE_NAME "kernel.cl" // Kernel file name
 #define KERNEL_RESIZE_GRAYSCALE "resize_and_grayscale"
+#define KERNEL_CALCZNCC "calc_zncc"
 
 int main() {
 	// Image width and height. Both input images are the same size
 	unsigned w = 2940;
 	unsigned h = 2016;
+	int window_y = 13;
+	int window_x = 11;
+	int min_disparity = 0;
+	int max_disparity = 65;
+	int neg_max_disparity = max_disparity * -1;
+	int threshold = 3;
+	// Create output format and object for the resized grayscaled image
+	int new_w = floor(w / 4);
+	int new_h = floor(h / 4);
+	// Grayscaled vectors
+	std::vector<unsigned char> im0_gray_vector(new_w * new_h);
+	std::vector<unsigned char> im1_gray_vector(new_w * new_h);
+	std::vector<unsigned char> dmap0(new_w * new_h);
+	std::vector<unsigned char> dmap1(new_w * new_h);
 	// Timing info
 	timer_struct timer;
 
@@ -45,8 +55,8 @@ int main() {
 	std::vector<unsigned char> im0_vector = charArrToVector(im0, w * h * 4);
 	std::vector<unsigned char> im1_vector = charArrToVector(im1, w * h * 4);
 	// Free the unsigned char arrays from memory
-	//delete im0;
-	//delete im1;
+	delete im0;
+	delete im1;
 	
 	/**********************************************/
 	/*				  OPENCL                      */
@@ -62,6 +72,17 @@ int main() {
 	cl_int err_num = NULL;
 	cl_uint num_of_platforms;
 	cl_uint num_of_devices;
+
+	// Timing varaibles
+	double milliseconds = 0;
+	cl_ulong opencl_start = 0, opencl_end = 0;
+
+	// For image object reading
+	size_t origin[3] = { 0, 0, 0 };
+	size_t region[3] = { new_w, new_h, 1 };
+	// Kernel workgroup sizes
+	size_t global_work_size[] = { new_w, new_h }; // Use the downscaled image height and width
+	size_t local_work_size[] = { 1, 1 };
 
 	// Load Kernel source into memory
 	kernel_source src = loadKernel(KERNEL_FILE_NAME);
@@ -95,56 +116,172 @@ int main() {
 	printf("Creating 2D images objects for im0 and im1\n");
 	cl_mem im0_cl = clCreateImage2D(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, &format, w, h, 0, &im0_vector[0], &err_num);
 	if (!errorCheck(err_num)) return 1;
-	//cl_mem im1_cl = clCreateImage2D(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, &format, w, h, 0, &im1_vector[0], &err_num);
-	//if (!errorCheck(err_num)) return 1;
-
-	// Create output format and object for the resized grayscaled image
-	int new_w = floor(w / 4);
-	int new_h = floor(h / 4);
-
-	cl_image_format format_gray;
-	format_gray.image_channel_order = CL_LUMINANCE; // The image was read as RGBA
-	format_gray.image_channel_data_type = CL_UNORM_INT8;
-	cl_mem im0_grayscale = clCreateImage2D(context, CL_MEM_READ_WRITE, &format_gray, new_w, new_h, 0, NULL, &err_num);
-	//cl_mem im0_grayscale = clCreateBuffer(context, CL_MEM_READ_WRITE, w * h * sizeof(unsigned char), NULL, &err_num);
+	cl_mem im1_cl = clCreateImage2D(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, &format, w, h, 0, &im1_vector[0], &err_num);
 	if (!errorCheck(err_num)) return 1;
-	//cl_mem im1_grayscale = clCreateImage2D(context, CL_MEM_WRITE_ONLY, &format, new_w, new_h, 0, NULL, &err_num);
-	//if (!errorCheck(err_num)) return 1;
+
+	// Create format for grayscale image objects
+	cl_image_format format_gray;
+	format_gray.image_channel_order = CL_LUMINANCE; // The image was is in grayscale
+	format_gray.image_channel_data_type = CL_UNORM_INT8;
+
+	cl_mem im0_grayscale = clCreateImage2D(context, CL_MEM_READ_WRITE, &format_gray, new_w, new_h, 0, NULL, &err_num);
+	if (!errorCheck(err_num)) return 1;
+	cl_mem im1_grayscale = clCreateImage2D(context, CL_MEM_WRITE_ONLY, &format_gray, new_w, new_h, 0, NULL, &err_num);
+	if (!errorCheck(err_num)) return 1;
+
+	printf("\n");
+
+	/****
+	* Resize + Grayscale im0
+	****/
 
 	// Give parameters to Kernel
-	printf("Passing parameters to the Kernel\n");
 	err_num = clSetKernelArg(kernel, 0, sizeof(cl_mem), &im0_cl);
 	err_num |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &im0_grayscale);
 	if (!errorCheck(err_num)) return 1;
 
-	size_t global_work_size[] = { new_w, new_h }; // 32, 32 because Device Max Work Group Size is 1024
-	size_t local_work_size[] = { 1, 1 }; // 32, 32 because Device Max Work Item Size is 1024 // IF YOU CHANGES THIS T 16, 16, e.g. THIS SHIT STOPS WORKING AT clEnqueueNDRangeKernel
-
 	// Execute the Kernel
-	printf("Executing the resize and grayscale Kernel\n");
+	printf("Executing the resize and grayscale Kernel for im0\n");
 	err_num = clEnqueueNDRangeKernel(cmd_q, kernel, 2, NULL, global_work_size, local_work_size, 0, NULL, &event);
 	if (!errorCheck(err_num)) return 1;
-
 	// Wait for execution to finish
 	clWaitForEvents(1, &event);
-	cl_ulong opencl_start = 0, opencl_end = 0;
+	opencl_start = 0, opencl_end = 0;
 	// Calculate execution time
 	clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &opencl_start, NULL);
 	clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &opencl_end, NULL);
-	double milliseconds = (cl_double)(opencl_end - opencl_start) * (cl_double)(1e-06);
+	milliseconds = (cl_double)(opencl_end - opencl_start) * (cl_double)(1e-06);
 	printf("Kernel execution done, took %f milliseconds\n", milliseconds);
 	
-	
 	// Get the resulting grayscaled image
-	size_t origin[3] = { 0, 0, 0 };
-	size_t region[3] = { new_w, new_h, 1 };
-	std::vector<unsigned char> im0_gray_vector(new_w * new_h);
 	err_num = clEnqueueReadImage(cmd_q, im0_grayscale, CL_TRUE, origin, region, 0, 0, &im0_gray_vector[0], 0, NULL, NULL);
-	//err_num = clEnqueueReadBuffer(cmd_q, im0_grayscale, CL_TRUE, 0, w * h * sizeof(unsigned char), &im0_gray_vector[0], 0, NULL, NULL);
 
-	lodepng::encode("imgs/test.png", im0_gray_vector, new_w, new_h, LCT_GREY);
-	//WriteImage("imgs/Resize_test.png", im0_gray_vector, new_w, new_h, LCT_GREY, 0);
+	if (WriteImage("imgs/im0_gray.png", im0_gray_vector, new_w, new_h, LCT_GREY, 8)) return 1;
 	
+	/****
+	* Resize + Grayscale im1
+	****/
+
+	err_num = clSetKernelArg(kernel, 0, sizeof(cl_mem), &im1_cl);
+	err_num |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &im1_grayscale);
+	if (!errorCheck(err_num)) return 1;
+
+	// Execute the Kernel
+	printf("Executing the resize and grayscale Kernel for im0\n");
+	err_num = clEnqueueNDRangeKernel(cmd_q, kernel, 2, NULL, global_work_size, local_work_size, 0, NULL, &event);
+	if (!errorCheck(err_num)) return 1;
+	// Wait for execution to finish
+	clWaitForEvents(1, &event);
+	opencl_start = 0, opencl_end = 0;
+	// Calculate execution time
+	clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &opencl_start, NULL);
+	clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &opencl_end, NULL);
+	milliseconds = (cl_double)(opencl_end - opencl_start) * (cl_double)(1e-06);
+	printf("Kernel execution done, took %f milliseconds\n", milliseconds);
+
+	// Get the resulting grayscaled image
+	err_num = clEnqueueReadImage(cmd_q, im1_grayscale, CL_TRUE, origin, region, 0, 0, &im1_gray_vector[0], 0, NULL, NULL);
+
+	if (WriteImage("imgs/im1_gray.png", im1_gray_vector, new_w, new_h, LCT_GREY, 8)) return 1;
+
+	/*
+	* Free memory
+	*/
+
+	FreeImageVector(im0_vector);
+	FreeImageVector(im1_vector);
+
+	err_num = clReleaseMemObject(im0_cl);
+	err_num |= clReleaseMemObject(im1_cl);
+	err_num |= clReleaseMemObject(im0_grayscale);
+	err_num |= clReleaseMemObject(im1_grayscale);
+	if (!errorCheck(err_num)) return 1;
+
+	/**********************************************/
+	/*				  CalcZNCC                    */
+	/**********************************************/
+
+	// Create Kernel
+	kernel = createKernel(context, device_id, KERNEL_CALCZNCC, (const char**)&src.source_str, (const size_t*)&src.source_size);
+	if (kernel == NULL) return 1;
+
+	// The resized + grayscaled images are now stored in im[0-1]_gray_vector
+	// Create Buffers for the vectors
+	cl_mem im0_new = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, new_w * new_h * sizeof(unsigned char), &im0_gray_vector[0], &err_num);
+	if (!errorCheck(err_num)) return 1;
+	cl_mem im1_new = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, new_w * new_h * sizeof(unsigned char), &im1_gray_vector[0], &err_num);
+	if (!errorCheck(err_num)) return 1;
+	cl_mem dmap0_cl = clCreateBuffer(context, CL_MEM_READ_WRITE, new_w * new_h * sizeof(unsigned char), NULL, &err_num);
+	if (!errorCheck(err_num)) return 1;
+	cl_mem dmap1_cl = clCreateBuffer(context, CL_MEM_READ_WRITE, new_w * new_h * sizeof(unsigned char), NULL, &err_num);
+	if (!errorCheck(err_num)) return 1;
+
+	/****
+	* im0 left + im1 right
+	****/
+	// Give parameters to Kernel
+	err_num = clSetKernelArg(kernel, 0, sizeof(cl_mem), &im0_new);
+	err_num |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &im1_new);
+	err_num |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &dmap0_cl);
+	err_num |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &new_w);
+	err_num |= clSetKernelArg(kernel, 4, sizeof(unsigned int), &new_h);
+	err_num |= clSetKernelArg(kernel, 5, sizeof(int), &window_y);
+	err_num |= clSetKernelArg(kernel, 6, sizeof(int), &window_x);
+	err_num |= clSetKernelArg(kernel, 7, sizeof(int), &min_disparity);
+	err_num |= clSetKernelArg(kernel, 8, sizeof(int), &max_disparity);
+	if (!errorCheck(err_num)) return 1;
+
+	// Execute the Kernel
+	printf("Executing the CalcZNCC Kernel for im0=left & im1=right\n");
+	err_num = clEnqueueNDRangeKernel(cmd_q, kernel, 2, NULL, global_work_size, local_work_size, 0, NULL, &event);
+	if (!errorCheck(err_num)) return 1;
+	// Wait for execution to finish
+	clWaitForEvents(1, &event);
+	opencl_start = 0, opencl_end = 0;
+	// Calculate execution time
+	clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &opencl_start, NULL);
+	clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &opencl_end, NULL);
+	milliseconds = (cl_double)(opencl_end - opencl_start) * (cl_double)(1e-06);
+	printf("Kernel execution done, took %f milliseconds\n", milliseconds);
+
+	err_num = clEnqueueReadBuffer(cmd_q, dmap0_cl, CL_TRUE, 0, new_w * new_h * sizeof(unsigned char), &dmap0[0], 0, NULL, NULL);
+	WriteImage("imgs/im0_disparity.png", dmap0, new_w, new_h, LCT_GREY, 8);
+
+	/****
+	* im1 left + im0 right
+	****/
+	// Give parameters to Kernel
+	err_num = clSetKernelArg(kernel, 0, sizeof(cl_mem), &im1_new);
+	err_num |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &im0_new);
+	err_num |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &dmap1_cl);
+	err_num |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &new_w);
+	err_num |= clSetKernelArg(kernel, 4, sizeof(unsigned int), &new_h);
+	err_num |= clSetKernelArg(kernel, 5, sizeof(int), &window_y);
+	err_num |= clSetKernelArg(kernel, 6, sizeof(int), &window_x);
+	err_num |= clSetKernelArg(kernel, 7, sizeof(int), &neg_max_disparity);
+	err_num |= clSetKernelArg(kernel, 8, sizeof(int), &min_disparity);
+	if (!errorCheck(err_num)) return 1;
+
+	// Execute the Kernel
+	printf("Executing the CalcZNCC Kernel for im0=left & im1=right\n");
+	err_num = clEnqueueNDRangeKernel(cmd_q, kernel, 2, NULL, global_work_size, local_work_size, 0, NULL, &event);
+	if (!errorCheck(err_num)) return 1;
+	// Wait for execution to finish
+	clWaitForEvents(1, &event);
+	opencl_start = 0, opencl_end = 0;
+	// Calculate execution time
+	clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &opencl_start, NULL);
+	clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &opencl_end, NULL);
+	milliseconds = (cl_double)(opencl_end - opencl_start) * (cl_double)(1e-06);
+	printf("Kernel execution done, took %f milliseconds\n", milliseconds);
+
+	err_num = clEnqueueReadBuffer(cmd_q, dmap1_cl, CL_TRUE, 0, new_w * new_h * sizeof(unsigned char), &dmap1[0], 0, NULL, NULL);
+	WriteImage("imgs/im1_disparity.png", dmap1, new_w, new_h, LCT_GREY, 8);
+
+	/**********************************************/
+	/*				  CrossCheck                  */
+	/**********************************************/
+
 
 
 
